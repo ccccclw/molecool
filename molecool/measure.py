37ba497ec23234e2b264016b384c1db8323dbabf
ccccclw <nkuclw@gmail.com>
2020-05-15 15:46:32 -0400

    functions

--- molecool/measure.py
+++ molecool/measure.py
@@ -6,8 +6,13 @@ This module is for functions
 import numpy as np
 
 def calculate_distance(rA, rB):
+
+    if isinstance(rA, np.ndarray) is False or isinstance(rB, np.ndarray) is False:
+        raise TypeError("input should be numpy array")
     d=(rA-rB)
     dist=np.linalg.norm(d)
+    if dist == 0.0:
+        raise Exception("Two atoms are in the same point")
     return dist
 
 def calculate_angle(rA, rB, rC, degrees=False):
